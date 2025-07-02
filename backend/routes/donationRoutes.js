5fdfc6d5ad0f9b1572cb74cf73b82061ac837871
sejal <sejipat@gmail.com>
2025-07-02 16:50:25 +0530

    Added getImageController

--- backend/routes/donationRoutes.js
+++ backend/routes/donationRoutes.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router();
 const postDonation = require('../controllers/postDonationController');
 const getDonation = require('../controllers/getDonationController');
+const getImage = require('../controllers/getImageController');
 router.post("/postDonation", postDonation);
 router.get("/getDonations", getDonation);
-  
+router.get("/image/:id", getImage); 
+
 module.exports = router;
