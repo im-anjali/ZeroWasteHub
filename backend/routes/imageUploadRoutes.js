c9eee23e76fa99d01ddb9b0ee55395f8ffc65024
Anjali Phule <anjaliphule5@gmail.com>
2025-07-02 11:46:17 +0530

    add: store image

---
+++ backend/routes/imageUploadRoutes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const { MongoClient, GridFSBucket } = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({storage});
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+router.post('/upload', upload.array('files'), async(req, res) =>{
+    try {
+        const client = await mongoClient.connect();
+        const db = client.db(process.env.DB_NAME);
+        const bucket = new GridFSBucket(db, {bucketName:'uploads'});
+        const fileDetails = req.body;
+        const extensions = JSON.parse(fileDetails.extensions || "[]");
+        const promises = req.files.map((file) =>{
+            const filename = file.originalname;
+            const uploadStream = bucket.openUploadStream(filename,{
+                metadata:{
+                    filename:filename
+                },
+                
+            })
+              return new Promise((resolve, reject) => {
+        uploadStream.on('finish', () => resolve(filename));
+        uploadStream.on('error', (err) => reject(err));
+        uploadStream.end(file.buffer);
+      });
+        })
+         const results = await Promise.all(promises);
+    client.close();
+    res.status(200).json({ message: 'Files uploaded successfully!', files: results });
+    } catch (error) {
+          console.error('Upload error:', error);
+    res.status(400).json({ message: error.message });
+    }
+})
+module.exports = router
