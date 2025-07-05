1d32b90ad9b9d24b11bcc8bfd927ee042a5521d1
sejal <sejipat@gmail.com>
2025-07-05 21:36:39 +0530

    Modified req routes

--- backend/routes/requestorRoutes.js
+++ backend/routes/requestorRoutes.js
@@ -1,20 +1,11 @@
-/*const express = require('express');
-const router = express.Router();
-const requestor = require('../controllers/requestController');
-const authenticate = require('../middleware/authmiddleware');
-router.get("/getDonations", requestor.getDonations);
-router.get("/image/:id", requestor.getImage); 
-router.post("/completeDonation",requestor.completeDonation);
-router.get('/myRequests', authenticate, requestor.getRequest);
-module.exports = router;*/
 const express = require('express');
 const router = express.Router();
 const requestor = require('../controllers/requestController');
-const authenticate = require('../middleware/authmiddleware'); // ✅ import like this
+const authenticate = require('../middleware/authmiddleware'); 
 
 router.get("/getDonations", requestor.getDonations);
 router.get("/image/:id", requestor.getImage);
 router.post("/completeDonation", requestor.completeDonation);
-router.get('/myRequests', authenticate, requestor.getRequest); // ✅ works only if authenticate is a function
+router.get('/myRequests', authenticate, requestor.getRequest);
 
 module.exports = router;
